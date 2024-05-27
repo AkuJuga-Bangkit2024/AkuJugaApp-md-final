cb7db651cc5f60d78dbec2190d2578b73a6d27c9
Hesoyaaam <ajisejati01@gmail.com>
2024-05-27 22:49:22 +0700

    add UI and viewmodel to ProfileFragment, edit camera layout

--- app/src/main/java/com/example/akujuga/view/ViewModelFactory.kt
+++ app/src/main/java/com/example/akujuga/view/ViewModelFactory.kt
@@ -7,6 +7,7 @@ import com.example.akujuga.data.UserRepository
 import com.example.akujuga.di.Injection
 import com.example.akujuga.view.login.LoginViewModel
 import com.example.akujuga.view.main.MainViewModel
+import com.example.akujuga.view.profile.ProfileViewModel
 
 class ViewModelFactory(private val repository: UserRepository): ViewModelProvider.NewInstanceFactory() {
 
@@ -18,6 +19,9 @@ class ViewModelFactory(private val repository: UserRepository): ViewModelProvide
             }
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
                 LoginViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(ProfileViewModel::class.java) -> {
+                ProfileViewModel(repository) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
--- app/src/main/java/com/example/akujuga/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/akujuga/view/profile/ProfileFragment.kt
@@ -1,51 +1,43 @@
 package com.example.akujuga.view.profile
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.akujuga.R
+import androidx.fragment.app.viewModels
+import com.example.akujuga.databinding.FragmentProfileBinding
+import com.example.akujuga.view.ViewModelFactory
+import com.example.akujuga.view.account.AccountActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel by viewModels<ProfileViewModel> {
+        ViewModelFactory.getInstance(requireContext())
     }
+
+    private var _binding : FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.account.setOnClickListener {
+            startActivity(Intent(requireContext(), AccountActivity::class.java))
+        }
+
+        binding.logout.setOnClickListener {
+            viewModel.logout()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
