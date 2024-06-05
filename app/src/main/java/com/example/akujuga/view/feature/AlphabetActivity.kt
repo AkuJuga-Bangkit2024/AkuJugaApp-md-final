a8169afd5a05d1396da1a6d67b53dac2745c84b1
Hesoyaaam <ajisejati01@gmail.com>
2024-06-05 22:41:35 +0700

    add item_layout for recyclerview & add binding

--- app/src/main/java/com/example/akujuga/view/feature/AlphabetActivity.kt
+++ app/src/main/java/com/example/akujuga/view/feature/AlphabetActivity.kt
@@ -1,21 +1,15 @@
 package com.example.akujuga.view.feature
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.akujuga.R
+import com.example.akujuga.databinding.ActivityAlphabetBinding
 
 class AlphabetActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlphabetBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_alphabet)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityAlphabetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
